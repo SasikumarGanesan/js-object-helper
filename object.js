0a92d7351a9f020e867f59151488da220920c1d6
shagi15 <sasi.pvm@gmail.com>
2019-02-24 14:22:28 +0530

    Initial commit.

---
+++ object.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {Object} object 
+ * @returns {number} 
+ */
+
+const objectLength = (object) => Object.keys(object).length;
+
+
+// module export
+
+module.export = objectLength;
